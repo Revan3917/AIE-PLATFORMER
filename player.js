766e52accd2a0625d2eefee4001be0cf40d952dc
Revan3917 <yodawise.MAN@Gmail.com>
2016-08-29 19:43:50 +1000

    New

--- player.js
+++ player.js
@@ -1,17 +1,47 @@
+var LEFT = 0;
+var RIGHT = 1;
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_IDLE_RIGHT = 3;
+var ANIM_JUMP_RIGHT = 4;
+var ANIM_WALK_RIGHT = 5;
+var ANIM_MAX = 6;
 
 
 var Player = function () {
-	this.image = document.createElement("img");
+	this.sprite = new Sprite("ChuckNorris.png");
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+		[0, 1, 2, 3, 4, 5, 6, 7]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+		[8, 9, 10, 11, 12]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+		[13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+		[52, 53, 54, 55, 56, 57, 58, 59]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+		[60, 61, 62, 63, 64]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+		[65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+	for (var i = 0; i < ANIM_MAX; i++) {
+		this.sprite.setAnimationOffset(i, -55, -87);
+	}
+
 	this.position = new Vector2();
 	this.position.set(9 * TILE, 0 * TILE);
 	this.width = 159;
 	this.height = 163;
-	this.offset = new Vector2();
-	this.offset.set(-55, -87);
+
 	this.velocity = new Vector2();
 	this.falling = true;
 	this.jumping = false;
-	this.image.src = "hero.png";
+	this.position = new Vector2();
+	this.position.set(9 * TILE, 0 * TILE);
+	this.velocity = new Vector2();
+	this.falling = true;
+	this.jumping = false;
+	this.direction = LEFT;
+
 };
 
 
@@ -54,40 +84,39 @@ Player.prototype.update = function (deltaTime) {
 	this.velocity.x = bound(this.velocity.x + (deltaTime * ddx), -MAXDX, MAXDX);
 	this.velocity.y = bound(this.velocity.y + (deltaTime * ddy), -MAXDY, MAXDY);
 	if ((wasleft && (this.velocity.x > 0)) ||
- 	(wasright && (this.velocity.x < 0)))
- {
- // clamp at zero to prevent friction from making us jiggle side to side
- this.velocity.x = 0;
- }
- // collision detection
- // Our collision detection logic is greatly simplified by the fact that the
- // player is a rectangle and is exactly the same size as a single tile.
- // So we know that the player can only ever occupy 1, 2 or 4 cells.
- // This means we can short-circuit and avoid building a general purpose
- // collision detection
- // engine by simply looking at the 1 to 4 cells that the player occupies:
- var tx = pixelToTile(this.position.x);
- var ty = pixelToTile(this.position.y);
- var nx = (this.position.x)%TILE; // true if player overlaps right
- var ny = (this.position.y)%TILE; // true if player overlaps below
- var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
- var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
- var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
- var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
- // If the player has vertical velocity, then check to see if they have hit a platform
- // below or above, in which case, stop their vertical velocity, and clamp their
- // y position:
- if (this.velocity.y > 0) {
-if ((celldown && !cell) || (celldiag && !cellright && nx)) {
- // clamp the y position to avoid falling into platform below
- this.position.y = tileToPixel(ty);
- this.velocity.y = 0; // stop downward velocity
- this.falling = false; // no longer falling
- this.jumping = false; // (or jumping)
- ny = 0; 
- // no longer overlaps the cells below
-}
- }
+		(wasright && (this.velocity.x < 0))) {
+		// clamp at zero to prevent friction from making us jiggle side to side
+		this.velocity.x = 0;
+	}
+	// collision detection
+	// Our collision detection logic is greatly simplified by the fact that the
+	// player is a rectangle and is exactly the same size as a single tile.
+	// So we know that the player can only ever occupy 1, 2 or 4 cells.
+	// This means we can short-circuit and avoid building a general purpose
+	// collision detection
+	// engine by simply looking at the 1 to 4 cells that the player occupies:
+	var tx = pixelToTile(this.position.x);
+	var ty = pixelToTile(this.position.y);
+	var nx = (this.position.x) % TILE; // true if player overlaps right
+	var ny = (this.position.y) % TILE; // true if player overlaps below
+	var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
+	var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
+	var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
+	var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
+	// If the player has vertical velocity, then check to see if they have hit a platform
+	// below or above, in which case, stop their vertical velocity, and clamp their
+	// y position:
+	if (this.velocity.y > 0) {
+		if ((celldown && !cell) || (celldiag && !cellright && nx)) {
+			// clamp the y position to avoid falling into platform below
+			this.position.y = tileToPixel(ty);
+			this.velocity.y = 0; // stop downward velocity
+			this.falling = false; // no longer falling
+			this.jumping = false; // (or jumping)
+			ny = 0;
+			// no longer overlaps the cells below
+		}
+	}
 	// abitrary choice for 1m
 	var METER = TILE;
 	// very exaggerated gravity (6x)
@@ -119,31 +148,31 @@ if ((celldown && !cell) || (celldiag && !cellright && nx)) {
 	var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
 	var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
 	var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
-	
+
 }
 else if (this.velocity.y < 0) {
-if ((cell && !celldown) || (cellright && !celldiag && nx)) {
-// clamp the y position to avoid jumping into platform above
-this.position.y = tileToPixel(ty + 1);
-this.velocity.y = 0; // stop upward velocity
-// player is no longer really in that cell, we clamped them to the cell below
-cell = celldown;
-cellright = celldiag; // (ditto)
-ny = 0; // player no longer overlaps the cells below
-}
+	if ((cell && !celldown) || (cellright && !celldiag && nx)) {
+		// clamp the y position to avoid jumping into platform above
+		this.position.y = tileToPixel(ty + 1);
+		this.velocity.y = 0; // stop upward velocity
+		// player is no longer really in that cell, we clamped them to the cell below
+		cell = celldown;
+		cellright = celldiag; // (ditto)
+		ny = 0; // player no longer overlaps the cells below
+	}
 }
 if (this.velocity.x > 0) {
-if ((cellright && !cell) || (celldiag && !celldown && ny)) {
-// clamp the x position to avoid moving into the platform we just hit
-this.position.x = tileToPixel(tx);
-this.velocity.x = 0; // stop horizontal velocity
-}
+	if ((cellright && !cell) || (celldiag && !celldown && ny)) {
+		// clamp the x position to avoid moving into the platform we just hit
+		this.position.x = tileToPixel(tx);
+		this.velocity.x = 0; // stop horizontal velocity
+	}
 }
 else if (this.velocity.x < 0) {
-if ((cell && !cellright) || (celldown && !celldiag && ny)) {
-// clamp the x position to avoid moving into the platform we just hit
-this.position.x = tileToPixel(tx + 1); this.velocity.x = 0; // stop horizontal velocity
-}
+	if ((cell && !cellright) || (celldown && !celldiag && ny)) {
+		// clamp the x position to avoid moving into the platform we just hit
+		this.position.x = tileToPixel(tx + 1); this.velocity.x = 0; // stop horizontal velocity
+	}
 }
 
 Player.prototype.draw = function () {
